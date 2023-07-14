6b14145f394672770c508f1a7832cf43fbf8839e
Karina <carianderrr@gmail.com>
2023-07-14 14:48:35 +0600

    Implement methods' logic in BaseRepository class; Change GetAllAsync method signature

---
+++ GamePlay.DAL/Repositories/BaseRepository.cs
@@ -0,0 +1,70 @@
+using System.Linq.Expressions;
+using GamePlay.DAL.Data;
+using GamePlay.Domain.Contracts;
+using GamePlay.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GamePlay.DAL.Repositories;
+
+public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : BaseEntity
+{
+    protected readonly ApplicationDbContext Context;
+    protected readonly DbSet<TEntity> DbSet;
+
+    protected BaseRepository(ApplicationDbContext context)
+    {
+        Context = context;
+        DbSet = context.Set<TEntity>();
+    }
+
+    public async Task<TEntity> AddAsync(TEntity entity)
+    {
+        var addedEntity = (await DbSet.AddAsync(entity)).Entity;
+        await Context.SaveChangesAsync();
+
+        return addedEntity;
+    }
+
+    public async Task<TEntity> DeleteAsync(TEntity entity)
+    {
+        var removedEntity = DbSet.Remove(entity).Entity;
+        await Context.SaveChangesAsync();
+
+        return removedEntity;
+    }
+
+    public async Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>>? predicate = null, params Expression<Func<TEntity, object>>[] includeProperties)
+    {
+        IQueryable<TEntity> query = DbSet;
+        foreach (var includeProperty in includeProperties)
+        {
+            query = query.Include(includeProperty);
+        }
+        if (predicate != null) return await query.Where(predicate).ToListAsync();
+        return await query.ToListAsync();
+    }
+
+    public async Task<TEntity> GetFirstAsync(Expression<Func<TEntity, bool>>? predicate = null, params Expression<Func<TEntity, object>>[] includeProperties)
+    {
+        IQueryable<TEntity> query = DbSet;
+        foreach (var includeProperty in includeProperties)
+        {
+            query = query.Include(includeProperty);
+        }
+        if (predicate != null) return (await query.Where(predicate).FirstOrDefaultAsync())!;
+        return (await query.FirstOrDefaultAsync())!;
+    }
+    
+    public async Task<TEntity> GetFirstAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        return (await DbSet.Where(predicate).FirstOrDefaultAsync())!;
+    }
+
+    public async Task<TEntity> UpdateAsync(TEntity entity)
+    {
+        DbSet.Update(entity);
+        await Context.SaveChangesAsync();
+
+        return entity;
+    }
+}
--- GamePlay.Domain/Contracts/IBaseRepository.cs
+++ GamePlay.Domain/Contracts/IBaseRepository.cs
@@ -7,9 +7,9 @@ public interface IBaseRepository<TEntity> where TEntity : BaseEntity
 {
     Task<TEntity> GetFirstAsync(Expression<Func<TEntity, bool>> predicate);
     Task<TEntity> GetFirstAsync(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includeProperties);
-    Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate);
     Task<TEntity> AddAsync(TEntity entity);
     Task<TEntity> UpdateAsync(TEntity entity);
     Task<TEntity> DeleteAsync(TEntity entity);
-    Task<IEnumerable<TEntity>> GetAllIncludingAsync(params Expression<Func<TEntity, object>>[] includeProperties);
+    Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>>? predicate = null, params Expression<Func<TEntity, object>>[] includeProperties);
+    
 }

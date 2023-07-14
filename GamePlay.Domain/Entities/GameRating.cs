5cb3e3afd2ebfc18a6539fa326f7a2d0a3b43752
Karina <carianderrr@gmail.com>
2023-07-14 16:10:00 +0600

    Fix id types to Guid

--- GamePlay.Domain/Entities/GameRating.cs
+++ GamePlay.Domain/Entities/GameRating.cs
@@ -2,9 +2,9 @@ namespace GamePlay.Domain.Entities;
 
 public class GameRating : BaseEntity
 {
-    public string? UserId { get; set; }
+    public Guid UserId { get; set; }
     public ApplicationUser? User { get; set; }
-    public string? GameId { get; set; }
+    public Guid GameId { get; set; }
     public Game? Game { get; set; }
     public int Rating { get; set; }
 }

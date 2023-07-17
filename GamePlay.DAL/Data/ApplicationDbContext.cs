1bf68c5d0d530793f764d1448e7e1c606bdf90ff
Karina <carianderrr@gmail.com>
2023-07-17 16:38:16 +0600

    Change DbContext and appsettings.json

--- GamePlay.DAL/Data/ApplicationDbContext.cs
+++ GamePlay.DAL/Data/ApplicationDbContext.cs
@@ -3,16 +3,17 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Duende.IdentityServer.EntityFramework.Options;
 using GamePlay.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace GamePlay.DAL.Data;
 
-public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+public class ApplicationDbContext :  IdentityDbContext<ApplicationUser>
 {
     public DbSet<Game>? Games { get; set; }
     public DbSet<UserRelation>? UserRelations { get; set; }
     public DbSet<GameRating>? GameRatings { get; set; }
-    public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
-        : base(options, operationalStoreOptions)
+    public ApplicationDbContext(DbContextOptions options)
+        : base(options)
     {
     }
 

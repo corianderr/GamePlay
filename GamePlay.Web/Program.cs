3cf044de2eebb111f2ad7a4bb41d7d174c7f0129
Karina <carianderrr@gmail.com>
2023-07-18 14:28:32 +0600

    Change default route and layout

--- GamePlay.Web/Program.cs
+++ GamePlay.Web/Program.cs
@@ -61,7 +61,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Games}/{action=Index}");
 app.MapRazorPages();
 
 app.Run();

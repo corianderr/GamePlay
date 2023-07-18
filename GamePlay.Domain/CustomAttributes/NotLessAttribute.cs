4a53fbbef2d3a0919b39deab5dcdd01024385ff7
Karina <carianderrr@gmail.com>
2023-07-18 15:21:48 +0600

    Add custom validation attributes

---
+++ GamePlay.Domain/CustomAttributes/NotLessAttribute.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace GamePlay.Domain.CustomAttributes;
+
+public class NotLessAttribute : ValidationAttribute
+{
+    private readonly string _comparisonProperty;
+
+    public NotLessAttribute(string comparisonProperty)
+    {
+        _comparisonProperty = comparisonProperty;
+    }
+
+    protected override ValidationResult? IsValid(object value, ValidationContext validationContext)
+    {
+        var property = validationContext.ObjectType.GetProperty(_comparisonProperty);
+
+        if (property == null)
+            throw new ArgumentException($"Property {_comparisonProperty} not found");
+
+        var comparisonValue = Convert.ToInt32(property.GetValue(validationContext.ObjectInstance));
+        var currentValue = (int)value;
+
+        return currentValue < comparisonValue ? new ValidationResult(ErrorMessage) : ValidationResult.Success;
+    }
+}
---
+++ GamePlay.Domain/CustomAttributes/YearRangeAttribute.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace GamePlay.Domain.CustomAttributes;
+
+public class YearRangeAttribute : ValidationAttribute
+{
+    private readonly int _minYear;
+
+    public YearRangeAttribute(int minYear)
+    {
+        _minYear = minYear;
+    }
+
+    protected override ValidationResult? IsValid(object value, ValidationContext validationContext)
+    {
+        if (value is not int year) return ValidationResult.Success;
+        var currentYear = DateTime.Now.Year;
+
+        if (year < _minYear || year > currentYear)
+        {
+            return new ValidationResult(ErrorMessage);
+        }
+        return ValidationResult.Success;
+    }
+}

c9862b96b7de921c6208aa50c71637192ae257b4
Karina <carianderrr@gmail.com>
2023-07-19 15:02:22 +0600

    Add Followers and Friends Counts in ApplicationUser Entity

--- GamePlay.DAL/MappingProfiles/UserProfile.cs
+++ GamePlay.DAL/MappingProfiles/UserProfile.cs
@@ -8,7 +8,9 @@ public class UserProfile : Profile
 {
     public UserProfile()
     {
-        CreateMap<CreateUserModel, ApplicationUser>();
+        CreateMap<CreateUserModel, ApplicationUser>()
+            .ForMember(g => g.FollowersCount, map => map.MapFrom(g => 0))
+            .ForMember(g => g.FriendsCount, map => map.MapFrom(g => 0));
         CreateMap<UserResponseModel, ApplicationUser>();
         CreateMap<ApplicationUser, UserResponseModel>();
         CreateMap<CreateUserRelationModel, UserRelation>();
--- GamePlay.Domain/Entities/ApplicationUser.cs
+++ GamePlay.Domain/Entities/ApplicationUser.cs
@@ -5,6 +5,8 @@ namespace GamePlay.Domain.Entities;
 public class ApplicationUser : IdentityUser
 {
     public string? PhotoPath { get; set; }
+    public int FriendsCount { get; set; }
+    public int FollowersCount { get; set; }
     public List<Game> Games { get; set; }
 
     public ApplicationUser() : base()

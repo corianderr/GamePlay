40efd8941e60f9bc84191b51dcbaf0a7aa509667
Karina <carianderrr@gmail.com>
2023-07-19 16:06:35 +0600

    Create edit user form and add method to display it in UsersController.cs

--- GamePlay.Web/Controllers/UsersController.cs
+++ GamePlay.Web/Controllers/UsersController.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using GamePlay.BLL.Services.Interfaces;
 using GamePlay.Domain.Models.User;
+using GamePlay.Web.Helpers;
 using GamePlay.Web.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNetCore.Authorization;
@@ -28,7 +30,7 @@ namespace GamePlay.Web.Controllers
             return View(users);
         }
 
-        // GET: Games/Details/5
+        // GET: Users/Details/5
         public async Task<IActionResult> Details(string id)
         {
             var userRelation = await _userService.GetRelationByUsersIdAsync(id, User.Identity.GetUserId());
@@ -39,6 +41,34 @@ namespace GamePlay.Web.Controllers
             relation.User = await _userService.GetFirstAsync(id);
             ViewBag.IsCurrentUser = User.Identity.GetUserId().Equals(relation.User.Id);
             return View(relation);
+        }
+
+        // GET: Users/Edit
+        public async Task<IActionResult> Edit()
+        {
+            var user = await _userService.GetFirstAsync(User.Identity.GetUserId());
+            return View(user);
+        }
+        
+        // POST: Games/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(UserResponseModel userModel, IFormFile? avatar)
+        {
+            try
+            {
+                if (!ModelState.IsValid) return View(userModel);
+
+                userModel.PhotoPath = await ImageUploadingHelper.ReuploadAndGetNewPathAsync("avatars",
+                    "/avatars/default-user-avatar.jpg", avatar, userModel.PhotoPath);
+                
+                var response = await _userService.UpdateAsync(User.Identity.GetUserId(), userModel);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }

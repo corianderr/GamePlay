8a487d8abf684fb4ead3c06be4c2bb9873bde825
Karina <carianderrr@gmail.com>
2023-07-19 15:16:55 +0600

    Implement logic to display views in UsersController

--- GamePlay.Web/Controllers/UsersController.cs
+++ GamePlay.Web/Controllers/UsersController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GamePlay.BLL.Services.Interfaces;
+using GamePlay.Domain.Models.User;
+using GamePlay.Web.Models;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,10 +21,24 @@ namespace GamePlay.Web.Controllers
             _userService = userService;
         }
 
+        // GET: Users
         public async Task<IActionResult> Index()
         {
             var users = await _userService.GetAllAsync();
             return View(users);
+        }
+
+        // GET: Games/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            var userRelation = await _userService.GetRelationByUsersIdAsync(id, User.Identity.GetUserId());
+
+            var relation = new UserRelationViewModel();
+            if (userRelation == null) relation.IsFriend = null;
+            
+            relation.User = await _userService.GetFirstAsync(id);
+            ViewBag.IsCurrentUser = User.Identity.GetUserId().Equals(relation.User.Id);
+            return View(relation);
         }
     }
 }

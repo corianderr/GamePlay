16d3cb873b1df8beb2902d1e54703e6201e84a60
Karina <carianderrr@gmail.com>
2023-07-19 10:23:26 +0600

    Change register page logic and refactor RegisterModel class

--- GamePlay.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ GamePlay.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,5 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
+
 #nullable disable
 
 using System;
@@ -29,67 +30,34 @@ namespace GamePlay.Web.Areas.Identity.Pages.Account
     public class RegisterModel : PageModel
     {
         private readonly ILogger<RegisterModel> _logger;
-        private readonly IEmailSender _emailSender;
         private readonly IUserService _userService;
 
         public RegisterModel(
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender,
             IUserService userService)
         {
             _logger = logger;
-            _emailSender = emailSender;
             _userService = userService;
         }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        [BindProperty]
-        public InputModel Input { get; set; }
+        [BindProperty] public InputModel Input { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         public string ReturnUrl { get; set; }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public IList<AuthenticationScheme> ExternalLogins { get; set; }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
+        
         public class InputModel
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
+                MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
@@ -105,25 +73,23 @@ namespace GamePlay.Web.Areas.Identity.Pages.Account
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
             returnUrl ??= Url.Content("~/");
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid) return Page();
+            
+            var user = CreateUser();
+            user.Email = Input.Email;
+            user.Username = Input.Email;
+            user.Password = Input.Password;
+            try
             {
-                var user = CreateUser(); 
-                user.Email = Input.Email;
-                user.Username = Input.Email;
-                user.Password = Input.Password;
-                try
-                {
-                    await _userService.RegisterAsync(user);
-                    _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
-                }
-                catch (BadRequestException e)
-                {
-                    ModelState.AddModelError(string.Empty, e.Message);
-                }
+                await _userService.RegisterAsync(user);
+                _logger.LogInformation("User logged in.");
+                return LocalRedirect(returnUrl);
+            }
+            catch (BadRequestException e)
+            {
+                ModelState.AddModelError(string.Empty, e.Message);
             }
 
-            // If we got this far, something failed, redisplay form
             return Page();
         }
 
@@ -136,8 +102,7 @@ namespace GamePlay.Web.Areas.Identity.Pages.Account
             catch
             {
                 throw new InvalidOperationException($"Can't create an instance of '{nameof(CreateUserModel)}'. " +
-                    $"Ensure that '{nameof(CreateUserModel)}' is not an abstract class and has a parameterless constructor, or alternatively " +
-                    $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
+                                                    $"Ensure that '{nameof(CreateUserModel)}' is not an abstract class and has a parameterless constructor.");
             }
         }
     }

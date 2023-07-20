4f786d4ef53f4d59b4afa032a67e580e4df1c205
Karina <carianderrr@gmail.com>
2023-07-20 17:30:19 +0600

    Change range validation values

--- GamePlay.Domain/Models/Game/UpdateGameModel.cs
+++ GamePlay.Domain/Models/Game/UpdateGameModel.cs
@@ -38,20 +38,21 @@ public class UpdateGameModel
     [DisplayName("Minimum age")]
     public int MinAge { get; set; }
 
-    [Range(1, 100, ErrorMessage = "Min play time value must be between 1 and 100")]
+    [Range(1, 90000, ErrorMessage = "Min play time value must be between 1 and 90000 minutes." +
+                                    "(No, you can not play more than 1500 hours straight, go and have a rest, see your family and world around)")]
     [Required(ErrorMessage = "Please enter minimum play time in minutes.")]
     [DisplayName("Minimum play time in minutes")]
     public int MinPlayTime { get; set; }
 
-    [Range(1, 100, ErrorMessage = "Max play time value must be between 1 and 100")]
+    [Range(1, 90000, ErrorMessage = "Max play time value must be between 1 and 90000 minutes. " +
+                                    "(No, you can not play more than 1500 hours straight, go and have a rest, see your family and world around)")]
     [Required(ErrorMessage = "Please enter maximum play time in minutes.")]
     [NotLess(nameof(MinPlayTime), ErrorMessage = "Maximum number must be greater than minimum number.")]
     [DisplayName("Maximum play time in minutes")]
     public int MaxPlayTime { get; set; }
 
     [YearRange(1800,
-        ErrorMessage =
-            "You definitely messed something up. Games in such a distant past have not yet been released. Enter a year between 1800 and the current year.")]
+        ErrorMessage = "You definitely messed something up. Games in such a distant past have not yet been released. Enter a year between 1800 and the current year.")]
     [Required(ErrorMessage = "Please enter year of release.")]
     [DisplayName("Year of Release")]
     public int YearOfRelease { get; set; }

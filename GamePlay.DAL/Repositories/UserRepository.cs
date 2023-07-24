6040b1c6c4b01dfee58e93a343feb18001a391d9
Karina <carianderrr@gmail.com>
2023-07-24 14:24:27 +0600

    Delete unnecessary methods in UserRepository

--- GamePlay.DAL/Repositories/UserRepository.cs
+++ GamePlay.DAL/Repositories/UserRepository.cs
@@ -23,52 +23,7 @@ public class UserRepository : BaseRepository<ApplicationUser>, IUserRepository
     //     user.Games.Add(game);
     //     await Context.SaveChangesAsync();
     // }
-
-    public async Task<UserRelation> AddSubscriptionAsync(UserRelation entity)
-    {
-        var addedEntity = (await DbRelationsSet.AddAsync(entity)).Entity;
-
-        // TODO: Try to delete Attach and Entry lines
-        var user = await GetFirstAsync(u => u.Id.Equals(entity.UserId));
-        DbSet.Attach(user);
-        user.FollowersCount++;
-        Context.Entry(user).Property(u => u.FollowersCount).IsModified = true;
-
-        await Context.SaveChangesAsync();
-
-        return addedEntity;
-    }
-
-    public async Task<UserRelation> BecomeFriendsAsync(ApplicationUser subscriber, ApplicationUser user)
-    {
-        var relation =
-            await DbRelationsSet.FirstOrDefaultAsync(r =>
-                r.SubscriberId.Equals(subscriber.Id) && r.UserId.Equals(user.Id));
-        relation.IsFriend = true;
-
-        await Context.SaveChangesAsync();
-        return relation;
-    }
-
-    public async Task<IEnumerable<UserRelation>> GetAllRelationsAsync(
-        Expression<Func<UserRelation, bool>>? predicate = null,
-        params Expression<Func<UserRelation, object>>[] includeProperties)
-    {
-        IQueryable<UserRelation> query = DbRelationsSet;
-        foreach (var includeProperty in includeProperties) query = query.Include(includeProperty);
-        if (predicate != null) return await query.Where(predicate).ToListAsync();
-        return await query.ToListAsync();
-    }
-
-    public async Task<UserRelation?> GetFirstRelationAsync(Expression<Func<UserRelation, bool>>? predicate = null,
-        params Expression<Func<UserRelation, object>>[] includeProperties)
-    {
-        IQueryable<UserRelation> query = DbRelationsSet;
-        foreach (var includeProperty in includeProperties) query = query.Include(includeProperty);
-        if (predicate != null) return await query.FirstOrDefaultAsync(predicate);
-        return await query.FirstOrDefaultAsync();
-    }
-
+    
     public bool IsEmailUnique(string email)
     {
         ApplicationUser? user = null;

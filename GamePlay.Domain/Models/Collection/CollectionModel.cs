cea191f83b2eabf07d3fdfd502bdc774e52241bf
Karina <carianderrr@gmail.com>
2023-07-25 14:31:29 +0600

    Add validation on collection entity

--- GamePlay.Domain/Models/Collection/CollectionModel.cs
+++ GamePlay.Domain/Models/Collection/CollectionModel.cs
@@ -2,11 +2,9 @@ using GamePlay.Domain.Entities;
 
 namespace GamePlay.Domain.Models.Collection;
 
-public class CollectionModel
+public class CollectionModel : CreateCollectionModel
 {
     public Guid Id { get; set; }
-    public string? Name { get; set; }
-    public string? UserId { get; set; }
     public ApplicationUser? User { get; set; }
     public List<Entities.Game>? Games { get; set; }
 }
--- GamePlay.Domain/Models/Collection/CreateCollectionModel.cs
+++ GamePlay.Domain/Models/Collection/CreateCollectionModel.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GamePlay.Domain.Models.Collection;
 
 public class CreateCollectionModel
 {
+    [Required(ErrorMessage = "Please enter collection name using less than 50 characters.")]
+    [StringLength(50)]
     public string? Name { get; set; }
     public string? UserId { get; set; }
 }

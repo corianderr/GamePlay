21a82be1ef5d193d0e1806b4820e16975e4176b1
Karina <carianderrr@gmail.com>
2023-07-25 11:16:50 +0600

    Add migrations

---
+++ GamePlay.DAL/Data/Migrations/20230725042026_ChangeNameFromResultToRound.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GamePlay.DAL.Migrations
+{
+    public partial class ChangeNameFromResultToRound : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Players_GameResults_GameResultId",
+                table: "Players");
+
+            migrationBuilder.DropTable(
+                name: "GameResults");
+
+            migrationBuilder.CreateTable(
+                name: "GameRounds",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    GameId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Place = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameRounds", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GameRounds_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameRounds_GameId",
+                table: "GameRounds",
+                column: "GameId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_GameRounds_GameResultId",
+                table: "Players",
+                column: "GameResultId",
+                principalTable: "GameRounds",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Players_GameRounds_GameResultId",
+                table: "Players");
+
+            migrationBuilder.DropTable(
+                name: "GameRounds");
+
+            migrationBuilder.CreateTable(
+                name: "GameResults",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    GameId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Place = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameResults", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GameResults_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameResults_GameId",
+                table: "GameResults",
+                column: "GameId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_GameResults_GameResultId",
+                table: "Players",
+                column: "GameResultId",
+                principalTable: "GameResults",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ GamePlay.DAL/Data/Migrations/20230725043934_ChangeColumnNamesInPlayers.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GamePlay.DAL.Migrations
+{
+    public partial class ChangeColumnNamesInPlayers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Players_GameRounds_GameResultId",
+                table: "Players");
+
+            migrationBuilder.RenameColumn(
+                name: "GameResultId",
+                table: "Players",
+                newName: "GameRoundId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Players_GameResultId",
+                table: "Players",
+                newName: "IX_Players_GameRoundId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_GameRounds_GameRoundId",
+                table: "Players",
+                column: "GameRoundId",
+                principalTable: "GameRounds",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Players_GameRounds_GameRoundId",
+                table: "Players");
+
+            migrationBuilder.RenameColumn(
+                name: "GameRoundId",
+                table: "Players",
+                newName: "GameResultId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Players_GameRoundId",
+                table: "Players",
+                newName: "IX_Players_GameResultId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_GameRounds_GameResultId",
+                table: "Players",
+                column: "GameResultId",
+                principalTable: "GameRounds",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

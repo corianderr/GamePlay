543779e5595582f21187df6440654efac7c96ced
Karina <carianderrr@gmail.com>
2023-07-25 16:31:21 +0600

    Add GameRoundObjectValidation

--- GamePlay.Domain/Models/GameRound/CreateGameRoundModel.cs
+++ GamePlay.Domain/Models/GameRound/CreateGameRoundModel.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GamePlay.Domain.Models.GameRound;
 
 public class CreateGameRoundModel
 {
+    [Required]
     public Guid GameId { get; set; }
+    [Required]
     public DateTime Date { get; set; }
+    [Required(ErrorMessage = "Please enter place name using less than 50 characters.")]
+    [StringLength(50)]
     public string? Place { get; set; }
 }

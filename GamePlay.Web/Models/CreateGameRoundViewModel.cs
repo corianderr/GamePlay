17a3563ff5ae6c682c9bdf23d9e5c8f8744eca14
Karina <carianderrr@gmail.com>
2023-07-26 10:34:49 +0600

    Add Create GameRound view

--- GamePlay.Web/Models/CreateGameRoundViewModel.cs
+++ GamePlay.Web/Models/CreateGameRoundViewModel.cs
@@ -6,6 +6,6 @@ namespace GamePlay.Web.Models;
 public class CreateGameRoundViewModel
 {
     public CreateGameRoundModel? GameRound { get; set; }
-    public IEnumerable<string>? PreviousPlaces { get; set; }
+    public IEnumerable<string?>? PreviousPlaces { get; set; }
     public IEnumerable<Player>? PreviousOpponents { get; set; }
 }

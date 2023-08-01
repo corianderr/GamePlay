d7defd29f90e5cb9fa6dee92fe8b045d581522e6
Karina <carianderrr@gmail.com>
2023-08-01 13:09:38 +0600

    Add ApiDependencyInjection

---
+++ GamePlay.API/ApiDependencyInjection.cs
@@ -0,0 +1,33 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace GamePlay.API;
+
+public static class ApiDependencyInjection
+{
+    public static void AddJwt(this IServiceCollection services, IConfiguration configuration)
+    {
+        var secretKey = configuration.GetValue<string>("JwtConfiguration:SecretKey");
+
+        var key = Encoding.ASCII.GetBytes(secretKey);
+
+        services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+    }
+}

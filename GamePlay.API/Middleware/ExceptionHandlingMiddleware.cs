6551323d38066d662e45d0df045e48a75fe81fe9
Karina <carianderrr@gmail.com>
2023-08-01 13:02:55 +0600

    Add ExceptionHandlingMiddleware

---
+++ GamePlay.API/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,51 @@
+using GamePlay.Domain.Exceptions;
+using GamePlay.Domain.Models;
+using Newtonsoft.Json;
+
+namespace GamePlay.API.Middleware;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleException(context, ex);
+        }
+    }
+
+    private Task HandleException(HttpContext context, Exception ex)
+    {
+        _logger.LogError(ex.Message);
+
+        var code = StatusCodes.Status500InternalServerError;
+        var errors = new List<string> { ex.Message };
+
+        code = ex switch
+        {
+            NotFoundException => StatusCodes.Status404NotFound,
+            BadRequestException => StatusCodes.Status400BadRequest,
+            _ => code
+        };
+
+        var result = JsonConvert.SerializeObject(ApiResult<string>.Failure(errors));
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = code;
+
+        return context.Response.WriteAsync(result);
+    }
+}

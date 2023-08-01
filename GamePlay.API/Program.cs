bcc1de08ef9dabfc326a68cf53c187fed069884d
Karina <carianderrr@gmail.com>
2023-08-01 13:10:46 +0600

    Set up project configuration

--- GamePlay.API/Program.cs
+++ GamePlay.API/Program.cs
@@ -1,11 +1,24 @@
+using GamePlay.API;
+using GamePlay.API.Filters;
+using GamePlay.API.Middleware;
+using GamePlay.BLL;
+using GamePlay.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(
+    config => config.Filters.Add(typeof(ValidateModelAttribute))
+);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDataAccess(builder.Configuration)
+    .AddApplication(builder.Environment);
+
+builder.Services.AddJwt(builder.Configuration);
 
 var app = builder.Build();
 
@@ -18,7 +31,16 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(corsPolicyBuilder =>
+    corsPolicyBuilder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+);
+
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.MapControllers();
 

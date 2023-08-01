d52cb08af5476099af30059478cc2bfd53b55915
Karina <carianderrr@gmail.com>
2023-08-01 13:19:40 +0600

    Move database and identity configuration settings to DataAccessDependencyInjection

--- GamePlay.DAL/DataAccessDependencyInjection.cs
+++ GamePlay.DAL/DataAccessDependencyInjection.cs
@@ -1,7 +1,11 @@
 using AutoMapper.Extensions.ExpressionMapping;
+using GamePlay.DAL.Data;
 using GamePlay.DAL.MappingProfiles;
 using GamePlay.DAL.Repositories;
 using GamePlay.Domain.Contracts.Repositories;
+using GamePlay.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -13,6 +17,8 @@ public static class DataAccessDependencyInjection
     {
         services.AddRepositories();
         services.RegisterAutoMapper();
+        services.AddDatabase(configuration);
+        services.AddIdentity();
         return services;
     }
 
@@ -30,5 +36,37 @@ public static class DataAccessDependencyInjection
     private static void RegisterAutoMapper(this IServiceCollection services)
     {
         services.AddAutoMapper(cfg => { cfg.AddExpressionMapping(); }, typeof(IMappingProfilesMarker));
+    }
+    
+    private static void AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+    }
+
+    private static void AddIdentity(this IServiceCollection services)
+    {
+        services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        services.Configure<IdentityOptions>(options =>
+        {
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequiredLength = 4;
+            options.Password.RequiredUniqueChars = 1;
+
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            options.Lockout.MaxFailedAccessAttempts = 5;
+            options.Lockout.AllowedForNewUsers = true;
+
+            options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+            options.User.RequireUniqueEmail = true;
+        });
     }
 }

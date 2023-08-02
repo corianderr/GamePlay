57cf0084500192ab8ef2c43a3815b3b4af6c6b73
Karina <carianderrr@gmail.com>
2023-08-02 13:09:45 +0600

    Implement all methods in CollectionController

---
+++ GamePlay.API/Controllers/CollectionController.cs
@@ -0,0 +1,75 @@
+using GamePlay.Domain.Contracts.Services;
+using GamePlay.Domain.Models;
+using GamePlay.Domain.Models.Collection;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamePlay.API.Controllers;
+
+[Authorize]
+public class CollectionController : ApiController
+{
+    private readonly ICollectionService _collectionService;
+
+    public CollectionController(ICollectionService collectionService)
+    {
+        _collectionService = collectionService;
+    }
+
+    // GET: Collections/Details/5
+    public async Task<ActionResult> Details(Guid id)
+    {
+        var collection = await _collectionService.GetByIdAsync(id);
+        return Ok(ApiResult<CollectionModel>.Success(collection));
+    }
+    
+    // POST: Collections/AddGame
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult> AddGame(Guid id, Guid collectionId)
+    {
+        await _collectionService.AddGameAsync(id, collectionId);
+        return Ok(ApiResult<BaseModel>.Success(new BaseModel(){Id = collectionId}));
+    }
+    
+    // POST: Collections/DeleteFromCollection
+    [HttpPost]
+    public async Task<ActionResult> DeleteFromCollection(Guid id, Guid collectionId)
+    {
+        await _collectionService.DeleteGameAsync(id, collectionId);
+        return Ok(ApiResult<BaseModel>.Success(new BaseModel(){Id = collectionId}));
+    }
+
+    // POST: Collections/Create
+    [HttpPost]
+    public async Task<ActionResult> Create(CreateCollectionModel collectionModel)
+    {
+        try
+        {
+            collectionModel.UserId = User.Identity.GetUserId();
+            await _collectionService.CreateAsync(collectionModel);
+        }
+        catch (ArgumentException ex)
+        {
+            ModelState.AddModelError("Name", ex.Message);
+        }
+        return Ok(ApiResult<CreateCollectionModel>.Success(collectionModel));
+    }
+    
+    // POST: Collections/Edit/5
+    [HttpPut("{id:guid}")]
+    public async Task<ActionResult> Edit(Guid id, CollectionModel collectionModel)
+    {
+        await _collectionService.UpdateAsync(id, collectionModel);
+        return Ok(ApiResult<BaseModel>.Success(new BaseModel{Id = id}));
+    }
+    
+    // POST: Collections/Delete/5
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult> DeleteConfirmed(Guid id)
+    {
+        await _collectionService.DeleteAsync(id);
+        return Ok(ApiResult<BaseModel>.Success(new BaseModel{Id = id}));
+    }
+}

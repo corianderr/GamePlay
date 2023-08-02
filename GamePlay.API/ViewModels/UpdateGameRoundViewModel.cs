666554094c89b70d2b5f7b18caf7995e6374e00c
Karina <carianderrr@gmail.com>
2023-08-02 12:43:25 +0600

    Add GameRound ViewModels

---
+++ GamePlay.API/ViewModels/UpdateGameRoundViewModel.cs
@@ -0,0 +1,12 @@
+using GamePlay.Domain.Models.GameRound;
+using GamePlay.Domain.Models.User;
+
+namespace GamePlay.API.ViewModels;
+
+public class UpdateGameRoundViewModel
+{
+    public GameRoundModel? GameRound { get; set; }
+    public IEnumerable<string?>? PreviousPlaces { get; set; }
+    public IEnumerable<string>? PreviousOpponents { get; set; }
+    public IEnumerable<UserModel>? Users { get; set; }
+}

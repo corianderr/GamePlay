2249133fd3eba850e96a61d4f39b1233a72f270d
Karina <carianderrr@gmail.com>
2023-08-03 14:31:49 +0600

    Add roles check on a routing level

--- GamePlay.UI/game-play-ui/src/App.js
+++ GamePlay.UI/game-play-ui/src/App.js
@@ -1,17 +1,27 @@
-import Register from './components/Register';
-import Login from './components/Login';
-import Home from './components/Home';
-import RequireAuth from './components/RequireAuth';
-import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Unauthorized from "./components/Unauthorized";
+import RequireAuth from "./components/RequireAuth";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+
+const ROLES = {
+  User: "user",
+  Admin: "admin",
+};
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="login" element={<Login />}/>
-        <Route path="register" element={<Register />}/>
-        <Route path="/" element={<Home />}/>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+          <Route path="/" element={<Home />} />
+        </Route>
       </Route>
     </Routes>
   );

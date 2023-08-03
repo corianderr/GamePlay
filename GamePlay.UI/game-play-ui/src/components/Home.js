277e1ba9214741f641d5d697221703a584544fe5
Karina <carianderrr@gmail.com>
2023-08-03 14:03:06 +0600

    Add Layout and RequireAuth components

---
+++ GamePlay.UI/game-play-ui/src/components/Home.js
@@ -0,0 +1,32 @@
+import { useNavigate, Link } from "react-router-dom";
+import { useContext } from "react";
+import AuthContext from "../context/AuthProvider";
+
+const Home = () => {
+    const { setAuth } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const logout = async () => {
+        // if used in more components, this should be in context 
+        // axios to /logout endpoint 
+        setAuth({});
+        navigate('/linkpage');
+    }
+
+    return (
+        <section>
+            <h1>Links</h1>
+            <br />
+            <h2>Public</h2>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+            <br />
+            <h2>Private</h2>
+            <Link to="/">Home</Link>
+            <Link to="/editor">Editors Page</Link>
+            <Link to="/admin">Admin Page</Link>
+        </section>
+    )
+}
+
+export default Home

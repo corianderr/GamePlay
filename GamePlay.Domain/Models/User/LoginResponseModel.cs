d1c357146bd2eb31cbcbf38c23752dba4f884a8d
Karina <carianderrr@gmail.com>
2023-08-04 15:07:05 +0600

    Add LoginResponseModel

--- GamePlay.Domain/Models/User/LoginResponseModel.cs
+++ GamePlay.Domain/Models/User/LoginResponseModel.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace GamePlay.Domain.Models.User;
 
 public class LoginResponseModel
@@ -5,6 +7,10 @@ public class LoginResponseModel
     public string? Username { get; set; }
     public string? Id { get; set; }
     public string? Email { get; set; }
-    public string? Token { get; set; }
+    public string? AccessToken { get; set; }
+    [JsonIgnore]
+    public string? RefreshToken { get; set; }
+    [JsonIgnore]
+    public DateTime RefreshTokenExpiryTime { get; set; }
     public IEnumerable<string>? Roles { get; set; }
 }

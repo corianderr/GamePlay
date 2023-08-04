393846135d71df0690c91cc3317256315ca6d3cd
Karina <carianderrr@gmail.com>
2023-08-04 15:06:31 +0600

    Add option to trust the device by adding persist state

--- GamePlay.UI/game-play-ui/src/components/Login.js
+++ GamePlay.UI/game-play-ui/src/components/Login.js
@@ -6,7 +6,7 @@ import axios from '../api/axios';
 const LOGIN_URL = '/User/login';
 
 const Login = () => {
-    const { setAuth } = useAuth();
+    const { setAuth, persist, setPersist } = useAuth();
 
     const navigate = useNavigate();
     const location = useLocation();
@@ -39,7 +39,7 @@ const Login = () => {
                 }
             );
             console.log(response?.data);
-            const accessToken = response?.data?.result.token;
+            const accessToken = response?.data?.result.accessToken;
             const roles = response?.data?.result.roles;
             const id = response?.data?.result.id;
             setAuth({ user, pwd, accessToken, roles, id });
@@ -59,6 +59,14 @@ const Login = () => {
             errRef.current.focus();
         }
     }
+
+    const togglePersist = () => {
+        setPersist(prev => !prev);
+    }
+
+    useEffect(() => {
+        localStorage.setItem("persist", persist);
+    }, [persist])
 
     return (     
         <section>
@@ -85,6 +93,15 @@ const Login = () => {
                     required
                 />
                 <button>Sign In</button>
+                <div className="persistCheck">
+                    <input
+                        type="checkbox"
+                        id="persist"
+                        onChange={togglePersist}
+                        checked={persist}
+                    />
+                    <label htmlFor="persist">Trust This Device</label>
+                </div>
             </form>
             <p>
                 Need an Account?<br />

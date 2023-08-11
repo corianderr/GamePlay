9c0bb875845c26bd8e9586e8ee7a31ba86ee9d2e
Karina <carianderrr@gmail.com>
2023-08-11 13:47:42 +0600

    Change axios response intercept

--- GamePlay.UI/game-play-ui/src/hooks/useAxiosPrivate.js
+++ GamePlay.UI/game-play-ui/src/hooks/useAxiosPrivate.js
@@ -3,49 +3,51 @@ import { useEffect } from "react";
 import useRefreshToken from "./useRefreshToken";
 import useAuth from "./useAuth";
 import { toast } from "react-toastify";
+import { isAxiosError } from "axios";
 
 const useAxiosPrivate = () => {
-    const refresh = useRefreshToken();
-    const { auth } = useAuth();
+  const refresh = useRefreshToken();
+  const { auth } = useAuth();
 
-    useEffect(() => {
-
-        const requestIntercept = axiosPrivate.interceptors.request.use(
-            config => {
-                if (!config.headers['Authorization']) {
-                    config.headers['Authorization'] = `Bearer ${auth?.accessToken}`;
-                }
-                return config;
-            }, (error) => Promise.reject(error)
-        );
-
-        const responseIntercept = axiosPrivate.interceptors.response.use(
-            response => response,
-            async (error) => {
-                const prevRequest = error?.config;
-                if (error?.response?.status === 403 && !prevRequest?.sent) {
-                    prevRequest.sent = true;
-                    const newAccessToken = await refresh();
-                    prevRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
-                    return axiosPrivate(prevRequest);
-                }
-                else if (error?.response?.status !== 200){
-                    console.log("RESPONSE ERROR")
-                    console.log(error?.response)
-                    console.log(error?.response?.data?.errors)
-                    toast.error(error?.response?.data?.errors);
-                }
-                return Promise.reject(error);
-            }
-        );
-
-        return () => {
-            axiosPrivate.interceptors.request.eject(requestIntercept);
-            axiosPrivate.interceptors.response.eject(responseIntercept);
+  useEffect(() => {
+    const requestIntercept = axiosPrivate.interceptors.request.use(
+      (config) => {
+        if (!config.headers["Authorization"]) {
+          config.headers["Authorization"] = `Bearer ${auth?.accessToken}`;
         }
-    }, [auth, refresh])
+        return config;
+      },
+      (error) => Promise.reject(error)
+    );
 
-    return axiosPrivate;
-}
+    const responseIntercept = axiosPrivate.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const prevRequest = error?.config;
+        if (error?.response?.status === 403 && !prevRequest?.sent) {
+          prevRequest.sent = true;
+          const newAccessToken = await refresh();
+          prevRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+          return axiosPrivate(prevRequest);
+        } else if (isAxiosError(error)) {
+            if (error?.response?.status >= 400 && error?.response?.status < 500) {
+                toast.error(JSON.stringify(error.response.data.errors).replace(/[{}[\]"]/g, ' '));
+            } else if (error?.response?.status >= 500) {
+                toast.error('Internal server error');
+            }
+            throw error;
+        }
+        return Promise.reject(error);
+      }
+    );
+
+    return () => {
+      axiosPrivate.interceptors.request.eject(requestIntercept);
+      axiosPrivate.interceptors.response.eject(responseIntercept);
+    };
+  }, [auth, refresh]);
+
+  return axiosPrivate;
+};
 
 export default useAxiosPrivate;

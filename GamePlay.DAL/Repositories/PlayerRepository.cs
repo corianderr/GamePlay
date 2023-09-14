b16e6e9d9d1d69685adbda5529766be44a72d79c
Karina <carianderrr@gmail.com>
2023-09-14 15:02:34 +0600

    Add implementation of repository and service contract for player

---
+++ GamePlay.DAL/Repositories/PlayerRepository.cs
@@ -0,0 +1,10 @@
+using GamePlay.DAL.Data;
+using GamePlay.Domain.Contracts.Repositories;
+using GamePlay.Domain.Entities;
+
+namespace GamePlay.DAL.Repositories; 
+
+public class PlayerRepository : BaseRepository<Player>, IPlayerRepository {
+    protected PlayerRepository(ApplicationDbContext context) : base(context) {
+    }
+}
